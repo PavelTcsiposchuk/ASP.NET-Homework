2faa92f195c86896275d3755ac3f5d9d05911f85
Pavel Tsiposchuk <pavelja60705@gmail.com>
2019-03-31 17:32:05 +0300

    .

---
+++ ASP.NET-Homework/Models/Task.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASP.NET_Homework.Models
+{
+    public class Task
+    {
+        int ID { get; set; }
+        bool Finished { get; set; }
+        string Body { get; set; }
+
+
+    
+    }
+}
